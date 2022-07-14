8713f82d2d736ef21242e71d73683b84ba5f72f2
khawingtien <106378720+khawingtien@users.noreply.github.com>
2022-07-14 13:09:46 +0200

    Rotation of endeffector at its own point (trocar)

--- main_code_trocar_khaw/Rotation_khaw.m
+++ main_code_trocar_khaw/Rotation_khaw.m
@@ -12,16 +12,51 @@ rod_length = 500; %mm
 rod_length_x= zeros(1,501);
 rod_length_y = 0:-1:-rod_length;
 
+%define endeffector square 
+bx = 17; %in mm
+by = 380; %in mm
+by_zero = 0; 
+% bz = 0.38;  %in m
+b30 = [bx  bx  -bx    -bx ;
+       by_zero  -by  -by   by_zero   ];
+%        0.12  0.12   0.12    0.12   -bz    -bz   -bz  -bz];
+% b30 = b30.*1000; %in mm
+b30_close = [b30  b30([1 2])'];%to close up the rectangle, to connect to the start point
+
+
 %calculate the 30° rotation for rod at the trocar (origin)
-angle = [60];
+angle = 30;
 A(:, 1) = rod_length_x; %rod length x
 A(:, 2) = rod_length_y; %rod length y
+
+%calculate rotation of endeffector right
+B_x = ones(380,1)*bx;
+B(:,1) = B_x;
+B_y = linspace(by_zero, -by, 380);
+B(:,2) = B_y;
+
+%calculate rotation of endeffector left
+C_x = ones(380,1)*-bx;
+C(:,1) = C_x;
+C_y = linspace(by_zero, -by, 380); %same as B_y
+C(:,2) = C_y;
+
 % Compute the rotation matrix.
 rotationMatrix = [cosd(angle) -sind(angle); sind(angle) cosd(angle)];
 % Rotate the coordinates.
 rotXY = A *rotationMatrix;
 u=rotXY(:, 1); %rotation matrix x-coordinate
 v=rotXY(:, 2); %rotation matrix y-coordinate
+
+rotXY_effector_right = B *rotationMatrix;
+u_effector_right =rotXY_effector_right(:,1);
+v_effector_right =rotXY_effector_right(:,2);
+
+rotXY_effector_left = C *rotationMatrix;
+u_effector_left =rotXY_effector_left(:,1);
+v_effector_left =rotXY_effector_left(:,2);
+
+
 
 %plot for visualisation
 figure
@@ -32,9 +67,13 @@ plot(trocar,'bo') %plor trocar point
 hold on
 plot(rod_length_x, rod_length_y,'r-','LineWidth', 2) %plot Rod in the original position (rotation = 0°)
 hold on
+plot(b30_close(1,:),b30_close(2,:),'r-','LineWidth', 2) %plot endeffecctor in the original position (rotation = 0°)
 axis equal
 hold on
 plot(u,v,'b-', 'LineWidth', 2) %plot Rod in rotation matrix  
+hold on 
+plot(u_effector_right,v_effector_right,'b-', 'LineWidth', 2) %plot endeffector_right in rotation matrix
+plot(u_effector_left,v_effector_left,'b-', 'LineWidth', 2) %plot endeffector_left in rotation matrix
 title(['Rotation of ' int2str(angle) ' degree at Origin'])
 xlabel('x-coordinate in mm') 
 ylabel('y-coordinate in mm') 
